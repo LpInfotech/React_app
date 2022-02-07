40035613ce67433495e513310e308c8615d0e076
Mukesh <msalaria@constellant.com>
2022-02-07 12:36:01 +0530

    react app

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import TableBody from './TableBody';
 import TableHeader from './TableHeader';
 
+
 export default class Table extends Component {
     render() {
         const {item} = this.props
